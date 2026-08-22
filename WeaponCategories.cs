72b920064326d2bca4d6747a00884ff4c9c28c57
j <twitch3dproductions@gmail.com>
2026-08-22 01:45:49 -0400

    Added Section 4.4 Weapon Categories system

---
+++ WeaponCategories.cs
@@ -0,0 +1,10 @@
+public enum WeaponCategory
+{
+    Ballistic,       // Bullets, cannons, railguns
+    Energy,          // Lasers, plasma, directed energy
+    Explosive,       // Grenades, artillery shells, demolition charges
+    Missile,         // Guided or unguided rockets
+    Support,         // Repair beams, buffs, debuffs
+    DirectFire,      // Hitscan or instant-impact weapons
+    AreaEffect       // Splash damage, AoE fields, shockwaves
+}
