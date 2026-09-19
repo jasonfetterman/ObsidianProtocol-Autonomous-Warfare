7450c716818d9d9e29e730717611242e0a9e80ca
j <twitch3dproductions@gmail.com>
2026-09-19 02:05:23 -0400

    Add basic squad formation spacing for group movement

--- Assets/Scripts/Command/UnitMovementManager.cs
+++ Assets/Scripts/Command/UnitMovementManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,6 +6,7 @@ public class UnitMovementManager : MonoBehaviour
 {
     [SerializeField] private Camera battlefieldCamera;
     [SerializeField] private UnitSelectionManager selectionManager;
+    [SerializeField] private float formationSpacing = 2f;
 
     private void Update()
     {
@@ -15,15 +17,45 @@ public class UnitMovementManager : MonoBehaviour
 
             if (Physics.Raycast(ray, out RaycastHit hit))
             {
-                foreach (SelectableUnit selected in selectionManager.SelectedUnits)
+                IReadOnlyList<SelectableUnit> selected = selectionManager.SelectedUnits;
+
+                if (selected.Count == 1)
                 {
-                    UnitMovement mover = selected.GetComponent<UnitMovement>();
-                    if (mover != null)
-                    {
-                        mover.MoveTo(hit.point);
-                    }
+                    MoveUnit(selected[0], hit.point);
+                }
+                else if (selected.Count > 1)
+                {
+                    MoveGroupInFormation(selected, hit.point);
                 }
             }
+        }
+    }
+
+    private void MoveUnit(SelectableUnit unit, Vector3 destination)
+    {
+        UnitMovement mover = unit.GetComponent<UnitMovement>();
+        if (mover != null)
+        {
+            mover.MoveTo(destination);
+        }
+    }
+
+    private void MoveGroupInFormation(IReadOnlyList<SelectableUnit> units, Vector3 center)
+    {
+        int count = units.Count;
+        int columns = Mathf.CeilToInt(Mathf.Sqrt(count));
+
+        for (int i = 0; i < count; i++)
+        {
+            int row = i / columns;
+            int col = i % columns;
+
+            float offsetX = (col - (columns - 1) / 2f) * formationSpacing;
+            float offsetZ = (row - (count / columns) / 2f) * formationSpacing;
+
+            Vector3 destination = center + new Vector3(offsetX, 0f, offsetZ);
+
+            MoveUnit(units[i], destination);
         }
     }
 }
