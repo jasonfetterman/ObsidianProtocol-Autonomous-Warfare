a481dbee57b3f21801ffcd0c6df5d720e14156ff
j <twitch3dproductions@gmail.com>
2026-09-19 09:20:34 -0400

    Add hover and selection highlighting for units

--- Assets/Scripts/Units/SelectableUnit.cs
+++ Assets/Scripts/Units/SelectableUnit.cs
@@ -5,19 +5,62 @@ public class SelectableUnit : MonoBehaviour
     [SerializeField]
     private bool isSelected;
 
+    [SerializeField]
+    private Color normalColor = Color.white;
+
+    [SerializeField]
+    private Color hoverColor = Color.yellow;
+
+    [SerializeField]
+    private Color selectedColor = Color.green;
+
+    private Renderer unitRenderer;
+    private bool isHovered;
+
     public bool IsSelected => isSelected;
+
+    private void Awake()
+    {
+        unitRenderer = GetComponent<Renderer>();
+        UpdateColor();
+    }
 
     public void Select()
     {
         isSelected = true;
-
-        Debug.Log($"{gameObject.name} Selected");
+        UpdateColor();
     }
 
     public void Deselect()
     {
         isSelected = false;
+        UpdateColor();
+    }
 
-        Debug.Log($"{gameObject.name} Deselected");
+    public void SetHovered(bool hovered)
+    {
+        isHovered = hovered;
+        UpdateColor();
+    }
+
+    private void UpdateColor()
+    {
+        if (unitRenderer == null)
+        {
+            return;
+        }
+
+        if (isSelected)
+        {
+            unitRenderer.material.color = selectedColor;
+        }
+        else if (isHovered)
+        {
+            unitRenderer.material.color = hoverColor;
+        }
+        else
+        {
+            unitRenderer.material.color = normalColor;
+        }
     }
 }
